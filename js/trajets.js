f77b33bb2f2c1236817881d7c43b3510a7136c79
Laurent <magrez0laurent@gmail.com>
2022-02-01 11:53:07 +0100

    late update: trajets principal, added temp photo, added button on hover

---
+++ js/trajets.js
@@ -0,0 +1,31 @@
+"use strict";
+
+// function pictureAndAnimChange() {
+//   let whichButton = document.getElementsByClassName("general__button");
+//   console.log(whichButton);
+//   if (whichButton.includes("general__button")) {
+//     console.log("ya bouton 1 dans l'array");
+//   } else {
+//     console.log("pas le bouton1");
+//   }
+
+// }
+
+// function pictureAnimChange() {
+//    let whichButton = document.getElementsByTagName("button");
+//    console.log("c'est un bouton");
+//    if (whichButton.classList.contains("button1-position")) {
+//      console.log("c'est bouton1");
+//    } else {
+//      console.log("pas le bouton1");
+//    }
+//  }
+
+
+//   let whichButton = document.getElementsByClassName("general__button");
+
+//   whichButton.addEventListener("click", checkWhichButton);
+
+//   function checkWhichButton() {
+//     alert(whichButton.classList);
+//   }
