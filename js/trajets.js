7b3ca3172ee41b1e41527056ae7fc30cbd192d36
Laurent <magrez0laurent@gmail.com>
2022-02-02 19:04:49 +0100

    js updated, buttons to change content are working, miss animations and effects

--- js/trajets.js
+++ js/trajets.js
@@ -1,41 +1,55 @@
 "use strict";
 
-// function pictureAndAnimChange() {
-//   let whichButton = document.getElementsByClassName("general__button");
-//   console.log(whichButton);
-//   if (whichButton.includes("general__button")) {
-//     console.log("ya bouton 1 dans l'array");
-//   } else {
-//     console.log("pas le bouton1");
-//   }
-// }
+let tulear = {
+  img: "img/trajets/temp_isole-madagascar_d.jpg.image.1296.974.high.jpg",
+  h: "Tulear",
+  p: "TLorem ipsum, dolor sit amet consectetur adipisicing elit. Minima rerum voluptatum iste nisi veritatis vel debitis perspiciatis cum, vitae totam! hic suscipit repellat aperiam explicabo",
+};
 
-// function testalert() {
-//   const element = document.getElementById("myBtn");
-//   element.addEventListener("click", function () {
-//     alert("click test");
-//   });
-// }
+let stAugustin = {
+  img: "img/trajets/temp_madagascar_d.jpg.image.1296.974.high.webp",
+  h: "St Augustin",
+  p: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Id possimus asperiores saepe recusandae hic suscipit repellat aperiam explicabo dolores minima. Sapiente, eos! Architecto facilis?",
+};
 
-// document.addEventListener("click", (checkWhichButton) => {
-//   let whichButton = checkWhichButton.target;
+let anakao = {
+  img: "img/trajets/temp_1-nosy-be-madagascar-timbuktu-travel-2.avif",
+  h: "Anakao",
+  p: "Id possimus asperiores saepe recusandae hic suscipit repellat aperiam explicabo dolores minima. Sapiente, eos! Architecto facilis nulla harum officia? Eaque, pariatur",
+};
 
-//   if (whichButton.tagName == "BUTTON") {
-//     console.log(checkWhichButton.target);
-//   }
-// });
+// target what's infront of eventlistener
 
-//function detectButton() {
-//   const mapAndButton = document.getElementById("mapwithbutton");
-//   mapAndButton.addEventListener("click", maFunctionName);
+function funcPrincipalChange(paramFunc) {
+  let whichButton = paramFunc.target;
+  let imgToChange = document.getElementById("trajets__principal-img");
+  let hToChange = document.getElementById("trajets__principal-h2");
+  let pToChange = document.getElementById("trajets__principal-p");
 
+  if (whichButton.tagName == "BUTTON") {
+    // console.log(whichButton);
 
-
-  function maFunctionName(paramFunc) {
-    let whichButton = paramFunc.target;
-
-    if (whichButton.tagName == "BUTTON") {
-      console.log(paramFunc.target);
+    if (whichButton.classList.contains("button1-position")) {
+      // console.log("c'est le bouton 1");
+      imgToChange.src = tulear.img;
+      hToChange.innerHTML = tulear.h;
+      pToChange.innerHTML = tulear.p;
+    } else {
+    }
+    if (whichButton.classList.contains("button2-position")) {
+      // console.log("c'est le bouton 2");
+      imgToChange.src = stAugustin.img;
+      hToChange.innerHTML = stAugustin.h;
+      pToChange.innerHTML = stAugustin.p;
+    } else {
+    }
+    if (whichButton.classList.contains("button3-position")) {
+      // console.log("c'est le bouton 3");
+      imgToChange.src = anakao.img;
+      hToChange.innerHTML = anakao.h;
+      pToChange.innerHTML = anakao.p;
+    } else {
     }
   }
-//}
+}
+
