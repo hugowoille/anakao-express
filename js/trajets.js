2db2a99423561cc434d00b1f92cd3d8475da28e8
Laurent <magrez0laurent@gmail.com>
2022-02-06 01:53:36 +0100

    updated JS regrouped functions, code simplification, fixed button positon on map

--- js/trajets.js
+++ js/trajets.js
@@ -1,6 +1,5 @@
 "use strict";
 
-
 // CONTENT------------------------------------------------------------------
 let imgTrajetsArray = [
   "img/trajets/temp_isole-madagascar_d.jpg.image.1296.974.high.jpg",
@@ -16,38 +15,9 @@ let pTrajetsArray = [
   "Id possimus asperiores saepe recusandae hic suscipit repellat aperiam explicabo dolores minima. Sapiente, eos! Architecto facilis nulla harum officia? Eaque, pariatur",
 ];
 
-
-
 // FUNCTION : ANNEXE------------------------------------------------------------------
-function getIndexToChangeContent(eIndexNum, cIndexNum) {
-  let imgToChange = document.querySelectorAll(".js-trajets__img");
-  let hToChange = document.querySelectorAll(".js-trajets__h2");
-  let pToChange = document.querySelectorAll(".js-trajets__p");
-  //list of all img, h2, p element to change content from,
-  // select them with nodelist index
-
-  imgToChange[eIndexNum].src = imgTrajetsArray[cIndexNum];
-  hToChange[eIndexNum].innerHTML = hTrajetsArray[cIndexNum];
-  pToChange[eIndexNum].innerHTML = pTrajetsArray[cIndexNum];
-}
-// ----------------------------------------------------
-function fadeInAndOutChangeContent(fadeclass, arg1, arg2) {
-  let articleToFade = document.querySelector(".js-principal-article");
-  articleToFade.classList.add(fadeclass);
-
-  setTimeout(function () {
-    getIndexToChangeContent(arg1, arg2);
-    articleToFade.classList.remove(fadeclass);
-  }, 150);
-}
-
-// ----------------------------------------------------
 // add effect on clicked button and remove it from others if they have it
-function addAndRemoveEffect(
-  clickedButton,
-  buttonClassToSkip,
-  classToAddAndRemove
-) {
+function addAndRemoveEffect(clickedButton, buttonClassToSkip, classToAddAndRemove) {
   clickedButton.classList.add(classToAddAndRemove);
 
   let buttonNodeList = document.querySelectorAll(".general__button");
@@ -61,9 +31,25 @@ function addAndRemoveEffect(
     }
   }
 }
+// ----------------------------------------------------
+function fadeInAndOutChangeContent(fadeclass, eIndexNum, cIndexNum, classSelector) {
+  let articleToFade = document.querySelector(classSelector);
+  articleToFade.classList.add(fadeclass);
+
+  setTimeout(function() {
+    let imgToChange = document.querySelectorAll(".js-trajets__img");
+    let hToChange = document.querySelectorAll(".js-trajets__h2");
+    let pToChange = document.querySelectorAll(".js-trajets__p");
+    //list of all img, h2, p element to change content from,
+    // select them with nodelist index
+    imgToChange[eIndexNum].src = imgTrajetsArray[cIndexNum];
+    hToChange[eIndexNum].innerHTML = hTrajetsArray[cIndexNum];
+    pToChange[eIndexNum].innerHTML = pTrajetsArray[cIndexNum];
+    articleToFade.classList.remove(fadeclass);
+  }, 150);
+}
 
 // FUNCTION : MAIN ------------------------------------------------------------------
-
 let stop = [-1];
 
 function funcPrincipalChange(paramFunc) {
@@ -74,35 +60,33 @@ function funcPrincipalChange(paramFunc) {
   let button2 = whichButton.classList.contains("button2-position");
 
   if (whichButton.tagName == "BUTTON") {
-
     if (button0 && stop[0] != 0) {
       stop[0] = 0;
-      console.log("button 0 = " + stop[0]);
+      // console.log("button 0 = " + stop[0]);
       addAndRemoveEffect(whichButton, "button0-position", "button-on-click");
-      fadeInAndOutChangeContent("trajet__principal-fade", 0, 0);
+      fadeInAndOutChangeContent(
+        "trajet__principal-fade", 0, 0, ".js-principal-article");
     }
 
     if (button1 && stop[0] != 1) {
       stop[0] = 1;
-      console.log("button 1 = " + stop[0]);
+      // console.log("button 1 = " + stop[0]);
       addAndRemoveEffect(whichButton, "button1-position", "button-on-click");
-      fadeInAndOutChangeContent("trajet__principal-fade", 0, 1);
-
+      fadeInAndOutChangeContent(
+        "trajet__principal-fade", 0, 1, ".js-principal-article");
     }
     if (button2 && stop[0] != 2) {
       stop[0] = 2;
-      console.log("button 2 = " + stop[0]);
+      // console.log("button 2 = " + stop[0]);
       addAndRemoveEffect(whichButton, "button2-position", "button-on-click");
-      fadeInAndOutChangeContent("trajet__principal-fade", 0, 2);
-
+      fadeInAndOutChangeContent(
+        "trajet__principal-fade", 0, 2, ".js-principal-article");
     }
   }
 }
 
 // articleToFade.opacity = "1";
 //     console.log(articleToFade.opacity);
-
-
 
 // let stoptest = -1;
 
@@ -137,5 +121,3 @@ function funcPrincipalChange(paramFunc) {
 //     }
 //   }
 // }
-
-// console.log("outside func " + stoptest);
